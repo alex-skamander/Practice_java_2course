61d6bcae841b2387e0ce911d781f86da5b6ecf1b
a.skamander <a.skamander@gmail.com>
2020-09-30 18:19:56 +0300

    fixed 7_1 practice

--- src/com/company/pr7_1/FurnitureTest.java
+++ src/com/company/pr7_1/FurnitureTest.java
@@ -11,17 +11,17 @@ public class FurnitureTest {
         Bed myBed = new Bed(12599, "черный", "IKEA","сталь",true, "лурой");
         myShop.addBedInShop(myBed);
         int i = myShop.getFurniture().indexOf(myBed);
-        System.out.println(myShop.getFurniture().get(i) + " - 1");
+        System.out.println(myShop.getFurniture().get(i));
 
         Table myTable = new Table(12999,"черный","IKEA","дерево",4,140,70,70);
         myShop.addTableInShop(myTable);
         int j = myShop.getFurniture().indexOf(myTable);
-        System.out.println(myShop.getFurniture().get(j) + " - 2");
+        System.out.println(myShop.getFurniture().get(j));
 
         Sofa mySofa = new Sofa(23999,"серый","IKEA","дерево",true,5);
         myShop.addSofaInShop(mySofa);
         int k = myShop.getFurniture().indexOf(mySofa);
-        System.out.println(myShop.getFurniture().get(k) + " - 3");
+        System.out.println(myShop.getFurniture().get(k));
 
         me.buyFurniture(myShop.getFurniture().get(i));
         me.buyFurniture(myShop.getFurniture().get(j));
